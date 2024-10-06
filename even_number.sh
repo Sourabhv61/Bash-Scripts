891fd66daede7104d8329ac35940c60f40a33018
Sourabhv61 <sourabverma615@gmail.com>
2024-10-06 16:15:34 +0530

    THis script will print event numbers

---
+++ even_number.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for i in {1..100}; do
+  if [ $(expr $i % 2) -eq 0 ]; then
+    echo $i
+  fi
+done
+
+#!/bin/bash
+for i in {1..100}; do
+  if [ $(expr $i % 2) -eq 0 ]; then
+    echo $i
+  fi
+done
+
